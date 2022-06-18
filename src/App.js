9450006889f6f13a33e5fdb01b95a09bdf5b070d
bryog <bryog0007@gmail.com>
2022-06-18 00:10:08 -0600

    proyecto de lista de tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import FreeCodeCamplogo from './imagenes/freecodecamp-logo.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-tarea">
+      <div className='lista'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas/>
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
